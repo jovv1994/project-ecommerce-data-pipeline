acfd9169b0733526f334343ee26b306f3ee01225
jovv1994 <jovv1994@outlook.com>
2025-12-23 09:06:09 -0500

    John Vásconez - Script revenue_per_state.sql

--- queries/revenue_per_state.sql
+++ queries/revenue_per_state.sql
@@ -3,3 +3,16 @@
 -- with most revenue and the second one the total revenue of each.
 -- HINT: All orders should have a delivered status and the actual delivery date 
 -- should be not null. 
+SELECT
+	oc.customer_state,
+	SUM(oop.payment_value) AS Revenue
+FROM olist_customers oc
+INNER JOIN olist_orders oo 
+ON oc.customer_id = oo.customer_id
+INNER JOIN olist_order_payments oop
+ON oop.order_id = oo.order_id 
+WHERE oo.order_status = 'delivered'
+AND oo.order_delivered_customer_date IS NOT NULL
+GROUP BY oc.customer_state
+ORDER BY Revenue DESC
+LIMIT 10;
